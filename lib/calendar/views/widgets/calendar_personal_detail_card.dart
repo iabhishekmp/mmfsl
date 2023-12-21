27ebc13386e45504ed34194172dbe956f6696ee2
ABHISHEK <iabhishekmp@gmail.com>
2023-12-22 03:58:19 +0530

    Minor change

--- lib/calendar/views/widgets/calendar_personal_detail_card.dart
+++ lib/calendar/views/widgets/calendar_personal_detail_card.dart
@@ -28,192 +28,209 @@ class CalendarPersonalDetailCard extends StatelessWidget {
     );
     return Obx(
       () {
+        final count = kCalendarController.users.value.byTab(tab).length;
         return kCalendarController.isFetching.value
             ? const Center(child: CircularProgressIndicator())
-            : ListView.separated(
-                padding: EdgeInsets.only(
-                  left: 24.w,
-                  right: 24.w,
-                  top: 24.h,
-                  bottom: 48.h,
-                ),
-                itemCount: kCalendarController.users.value.byTab(tab).length,
-                separatorBuilder: (context, index) {
-                  return SizedBox(height: 24.h);
-                },
-                itemBuilder: (context, index) {
-                  final users = kCalendarController.users.value.byTab(tab);
-                  final user = users[index];
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.r),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.grey.withOpacity(0.5),
+            : count == 0
+                ? Center(
+                    child: Text(
+                      'No data found!',
+                      style: boldTextStyle,
+                    ),
+                  )
+                : ListView.separated(
+                    padding: EdgeInsets.only(
+                      left: 24.w,
+                      right: 24.w,
+                      top: 24.h,
+                      bottom: 48.h,
+                    ),
+                    itemCount: count,
+                    separatorBuilder: (context, index) {
+                      return SizedBox(height: 24.h);
+                    },
+                    itemBuilder: (context, index) {
+                      final users = kCalendarController.users.value.byTab(tab);
+                      final user = users[index];
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        children: [
-                          //? name, priority & call
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
                             children: [
-                              //? name & priority
-                              Column(
+                              //? name, priority & call
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  //? name
-                                  Text(
-                                    user.name,
-                                    style: boldTextStyle,
-                                  ),
+                                  //? name & priority
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      //? name
+                                      Text(
+                                        user.name,
+                                        style: boldTextStyle,
+                                      ),
 
-                                  //? ID
-                                  Text(
-                                    'ID: ${user.id}',
-                                    style: normalTextStyle,
-                                  ),
+                                      //? ID
+                                      Text(
+                                        'ID: ${user.id}',
+                                        style: normalTextStyle,
+                                      ),
 
-                                  //? Offered
-                                  Text.rich(
-                                    TextSpan(
-                                      children: [
+                                      //? Offered
+                                      Text.rich(
                                         TextSpan(
-                                          text: 'Offered : ',
-                                          style: normalTextStyle,
+                                          children: [
+                                            TextSpan(
+                                              text: 'Offered : ',
+                                              style: normalTextStyle,
+                                            ),
+                                            TextSpan(
+                                              text: inr(user.offered),
+                                              style: boldTextStyle,
+                                            ),
+                                          ],
                                         ),
+                                      ),
+
+                                      //? Current
+                                      Text.rich(
                                         TextSpan(
-                                          text: inr(user.offered),
-                                          style: boldTextStyle,
+                                          children: [
+                                            TextSpan(
+                                              text: 'Current : ',
+                                              style: normalTextStyle,
+                                            ),
+                                            TextSpan(
+                                              text: inr(user.current),
+                                              style: boldTextStyle,
+                                            ),
+                                          ],
                                         ),
-                                      ],
-                                    ),
+                                      ),
+
+                                      //? Type
+                                      Text(
+                                        user.type.name,
+                                        style: normalTextStyle,
+                                      ),
+
+                                      //? Priority
+                                      Text(
+                                        '● ${user.priority.text}',
+                                        style: normalTextStyle.copyWith(
+                                          color: user.priority.color,
+                                        ),
+                                      ),
+                                    ],
                                   ),
 
-                                  //? Current
-                                  Text.rich(
-                                    TextSpan(
-                                      children: [
-                                        TextSpan(
-                                          text: 'Current : ',
-                                          style: normalTextStyle,
+                                  //? Call Button
+                                  Material(
+                                    elevation: 0.7,
+                                    shape: const CircleBorder(),
+                                    color: Colors.white,
+                                    child: InkWell(
+                                      borderRadius: BorderRadius.circular(
+                                        1000,
+                                      ),
+                                      onTap: () =>
+                                          MyLauncher.call(context, user.phone),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(16),
+                                        child: Icon(
+                                          Icons.call,
+                                          size: 30,
+                                          color: Theme.of(context).primaryColor,
                                         ),
-                                        TextSpan(
-                                          text: inr(user.current),
-                                          style: boldTextStyle,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-
-                                  //? Type
-                                  Text(
-                                    user.type.name,
-                                    style: normalTextStyle,
-                                  ),
-
-                                  //? Priority
-                                  Text(
-                                    '● ${user.priority.text}',
-                                    style: normalTextStyle.copyWith(
-                                      color: user.priority.color,
+                                      ),
                                     ),
                                   ),
                                 ],
                               ),
 
-                              //? Call Button
-                              Material(
-                                elevation: 0.7,
-                                shape: const CircleBorder(),
-                                color: Colors.white,
-                                child: InkWell(
-                                  borderRadius: BorderRadius.circular(
-                                    1000,
+                              //? divider
+                              const Divider(),
+
+                              //? day & date
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  //? Due Date
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Due Date',
+                                        style: normalTextStyle,
+                                      ),
+                                      Text(
+                                        dMMMyy(user.dueDate),
+                                        style: boldTextStyle.copyWith(
+                                          fontSize: 16.sp,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  onTap: () =>
-                                      MyLauncher.call(context, user.phone),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(16),
-                                    child: Icon(
-                                      Icons.call,
-                                      size: 30,
-                                      color: Theme.of(context).primaryColor,
-                                    ),
+
+                                  //? Level
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Level',
+                                        style: normalTextStyle,
+                                      ),
+                                      Text(
+                                        user.level.toString(),
+                                        style: boldTextStyle.copyWith(
+                                          fontSize: 16.sp,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
+
+                                  //? Days Left
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Days Left',
+                                        style: normalTextStyle,
+                                      ),
+                                      Text(
+                                        user.daysLeft.toString(),
+                                        style: boldTextStyle.copyWith(
+                                          fontSize: 16.sp,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ],
                           ),
-
-                          //? divider
-                          const Divider(),
-
-                          //? day & date
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              //? Due Date
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Due Date',
-                                    style: normalTextStyle,
-                                  ),
-                                  Text(
-                                    dMMMyy(user.dueDate),
-                                    style:
-                                        boldTextStyle.copyWith(fontSize: 16.sp),
-                                  ),
-                                ],
-                              ),
-
-                              //? Level
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Level',
-                                    style: normalTextStyle,
-                                  ),
-                                  Text(
-                                    user.level.toString(),
-                                    style:
-                                        boldTextStyle.copyWith(fontSize: 16.sp),
-                                  ),
-                                ],
-                              ),
-
-                              //? Days Left
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Days Left',
-                                    style: normalTextStyle,
-                                  ),
-                                  Text(
-                                    user.daysLeft.toString(),
-                                    style:
-                                        boldTextStyle.copyWith(fontSize: 16.sp),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                        ),
+                      );
+                    },
                   );
-                },
-              );
       },
     );
   }
